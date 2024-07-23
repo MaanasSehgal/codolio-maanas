3b6c342f3c37bceb2d21f236c2cb09139beb2ccc
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-07-23 12:15:42 +0530

    Changed type filter and fixed a bug

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -175,7 +175,7 @@ const Transactions: React.FC = () => {
                         className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${typeFilter !== "" ? "bg-green-500 dark:bg-green-900 text-white" : ""}`}
                         onChange={handleFilterChange(setTypeFilter)}
                     >
-                        <option value="Type">Type</option>
+                        <option value="">Type</option>
                         <option value="Expense">Expense</option>
                         <option value="Income">Income</option>
                     </select>
